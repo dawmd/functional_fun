39e600ae45f99810ffa6811a38938186400a15e5
dawmd <dawmd55@gmail.com>
2021-05-05 18:32:38 +0200

    Rewritten some functions

--- blockchain/main.ml
+++ blockchain/main.ml
@@ -25,35 +25,6 @@ type block = blockdata * hashcode
    how to get it in ocaml *)
 let sha256 x = "0000000000000000"
 
-(* the number of zeroes at the beginning of a "good" hashcode *)
-let num_of_zeros = 4
-
-(* examines if the given hash is "good" *)
-let check_hash hash =
-  if String.length hash < num_of_zeros then
-    false
-  else
-    let rec aux index =
-      if index >= num_of_zeros then
-        true
-      else if String.get hash index = '0' then
-        aux (index + 1)
-      else
-        false
-    in aux 0
-
-(* finds the value of nonce the given block should have *)
-let find_nonce (block : block) =
-  let data = fst block in
-  let rec aux curr_nonce =
-    let tmp_data = {data with nonce = curr_nonce} in
-    let tmp_hash = sha256 tmp_data in
-    if check_hash tmp_hash then
-      curr_nonce
-    else
-      aux (curr_nonce + 1)
-  in aux 0
-
 let set_num (block : block) (new_num : int) : block =
   let new_data = {(fst block) with num_of_block = new_num} in
   let new_hash = sha256 new_data in
@@ -96,23 +67,28 @@ let add_transaction_to_block (block : block) (new_tran : transaction) : block =
 
 
 
-type blockchain = block list * int
+type blockchain = {blocks : block list; length : int}
 
-let empty_chain : blockchain = ([], 0)
+let empty_chain : blockchain = {blocks = []; length = 0}
 
-let push_back (block : block) (chain : blockchain) : blockchain =
-  let ls = fst chain in
-  match ls with
-  | [] -> (block :: [], 1)
-  | h :: t ->
-    let previous_hash = snd h in
-    let new_data = {(fst block) with prev_hash = Some previous_hash} in
-    let new_nonce = find_nonce (new_data, snd block) in
-    let new_data = {new_data with nonce = new_nonce} in
-    ((new_data, sha256 new_data) :: ls, snd chain + 1)
+(* the number of zeroes at the beginning of a "good" hashcode *)
+let num_of_zeros = 4
 
-let is_chain_empty (chain : blockchain) = chain = ([], 0)
+(* examines if the given hash is "good" *)
+let check_hash hash =
+  if String.length hash < num_of_zeros then
+    false
+  else
+    let rec aux index =
+      if index >= num_of_zeros then
+        true
+      else if String.get hash index = '0' then
+        aux (index + 1)
+      else
+        false
+    in aux 0
 
+(*
 let is_chain_correct (chain : blockchain) =
   if is_chain_empty chain then true else
   let rec aux ls =
@@ -129,20 +105,97 @@ let is_chain_correct (chain : blockchain) =
           false
         else
           aux tail in
-  aux (fst chain)
+  aux chain.blocks
 
-let chain_length (chain : blockchain) : int = snd chain
 
-let get_nth_block (chain : blockchain) (num_of_block : int) : block =
-  if chain_length chain < num_of_block || num_of_block < default_num then
-    assert false
+let is_chain_correct (chain : blockchain) =
+  if is_chain_empty chain then true else
+  let rec aux ls curr_num =
+    let head = List.hd ls in
+    if (fst head).num_of_block <> curr_num || not (check_hash (snd head)) then
+      false
+    else match List.tl ls with
+    | [] -> (fst head).prev_hash = None
+    | next_head :: t ->
+
+
+
+      let prev_hash = snd (List.hd t) in
+      match 
+
+  aux chain.blocks (chain.length + default_num - 1)
+*)
+
+type blockchain = { blocks : block list; length : int }
+
+let empty_chain : blockchain = { blocks = []; length = 0 }
+
+let is_chain_empty (chain : blockchain) = chain = empty_chain
+
+(* the number of zeroes at the beginning of a "good" hashcode *)
+let num_of_zeros = 4
+
+(* examines if the given hash is "good" *)
+let check_hash (hash : hashcode) : bool =
+  if String.length hash < num_of_zeros then
+    false
   else
-    let rec aux num ls =
-      if num = num_of_block then
-        List.hd ls
+    let rec aux index =
+      if index >= num_of_zeros then
+        true
+      else if String.get hash index = '0' then
+        aux (index + 1)
+      else
+        false
+    in aux 0
+
+let check_block_hash (block : block) : bool =
+  let blocks_hash = sha256 (fst block) in
+  snd block = blocks_hash && check_hash blocks_hash
+
+let append_block (block : block) (chain : blockchain) : blockchain option =
+  if is_chain_empty chain then
+    if check_block_hash block && (fst block).num_of_block = default_num then
+      Some { blocks = block :: []; length = 1 }
+    else
+      None
+  else
+    let fst_block = List.hd chain.blocks in
+    if check_block_hash block
+    && (fst block).num_of_block = (fst fst_block).num_of_block + 1
+    && snd fst_block = (fst block).prev_hash then
+      Some { blocks = block :: chain.blocks; length = chain.length + 1 }
+    else
+      None
+
+(* finds the value of nonce the given block should have *)
+let find_nonce (block : block) =
+  let data = fst block in
+  let rec aux curr_nonce =
+    let tmp_data = { data with nonce = curr_nonce } in
+    let tmp_hash = sha256 tmp_data in
+    if check_hash tmp_hash then
+      curr_nonce
+    else
+      aux (curr_nonce + 1)
+  in aux 0
+
+let mine (chain : blockchain) (block : block) =
+  let last_block = List.hd chain.blocks in
+  let last_num = (fst last_block).num_of_block
+  and prev_hash = snd last_block in
+  let block = set_prev_hash (set_num block (last_num + 1)) prev_hash in
+  let new_nonce = find_nonce block in
+  let new_block = { (fst block) with nonce = new_nonce } in
+  (new_block, sha256 new_block)
+
+let get_nth_block (chain : blockchain) (num_of_block : int) : block option =
+  if chain_length chain + default_num - 1 < num_of_block || num_of_block < default_num then
+    None
+  else
+    let rec aux curr_num ls =
+      if num = curr_num then
+        Some (List.hd ls)
       else
         aux (num - 1) (List.tl ls) in
-    aux (chain_length chain) (fst chain)
-
-
-(* TO DO: add comments, update the mli file *)
+    aux ((chain_length chain) + default_num - 1) chain.blocks
