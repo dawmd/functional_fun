6a68e246049b6d1b661d966486649f3108e6c5b6
dawmd <dawmd55@gmail.com>
2021-05-07 13:50:08 +0200

    Added some comments, updated the mli file

--- blockchain/main.mli
+++ blockchain/main.mli
@@ -8,10 +8,6 @@ val add_transaction : register -> transaction -> register
 
 type block
 
-(* returns the register a block consists of *)
-val get_reg : block -> register
-(* replaces the register of a block with a new one *)
-val set_reg : block -> register -> block
 (* creates a new, empty block consisting of a register *)
 val create_block : register -> block
 (* adds a new transaction to a block *)
@@ -21,12 +17,16 @@ type blockchain
 
 (* an empty blockchain *)
 val empty_chain : blockchain
-(* attaches a new block to a blockchain *)
-val push_back : block -> blockchain -> blockchain
-(* examines if a blockchain consists of correctly attached blocks *)
-val is_chain_correct : blockchain -> bool
-(* returns a blockchain's size -- the number of blocks
-   it consists of *)
-val chain_length : blockchain -> int
+(* checks if a blockchain is empty *)
+val is_chain_empty : blockchain -> bool
+(* attaches a new block at the end of a blockchain *)
+val append_block : block -> blockchain -> blockchain
+(* modifies a given block so it can be attached to a given blockchain *)
+val mine : blockchain -> block -> block
 (* returns the n-th block in a blockchain *)
 val get_nth_block : blockchain -> int -> block
+(* examines if a blockchain consists of correctly attached blocks *)
+val is_chain_correct : blockchain -> bool
+(* returns a blockchain's length -- the number of blocks
+   it consists of *)
+val chain_length : blockchain -> int
